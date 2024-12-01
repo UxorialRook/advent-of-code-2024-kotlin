72adbc17001b564d1a3847c6ff7f9a168534621a
Matthieu Mabillard <matthieu.mabillard@gmail.com>
2024-12-01 11:45:52 +0100

    feat(day1): Part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,57 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>();
+        val list2 = mutableListOf<Int>();
+        input.iterator().forEach {
+            val temp = it.split("   ");
+            list1.add(temp[0].toInt())
+            list2.add(temp[1].toInt())
+        }
+
+
+        val sortedList1 = list1.sorted()
+        val sortedList2 = list2.sorted()
+
+        return sortedList1.zip(sortedList2) { elList1, elList2 ->
+            abs(elList1 - elList2)
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val list1 = mutableListOf<Int>();
+        val list2 = mutableListOf<Int>();
+        input.iterator().forEach {
+            val temp = it.split("   ");
+            list1.add(temp[0].toInt())
+            list2.add(temp[1].toInt())
+        }
+
+        // mapResult if the map that we will reduce to find the final answer
+        // mapNumberTime contains how many time we found the element in the left list for
+        // the final calculation
+        val mapResult = list1.associateWith {0}.toMutableMap()
+        val mapNumberTime = list1.associateWith { 0 }.toMutableMap()
+
+        list1.iterator().forEach {
+            mapNumberTime[it] = (mapNumberTime[it] ?: 0) + 1
+        }
+
+        list2.iterator().forEach {
+            if(mapResult.containsKey(it)) {
+                mapResult[it] = (mapResult[it] ?: 0) + 1
+            }
+        }
+
+        // key (the left value) * value (number time in the right) * number time in the left
+        return mapResult.entries.map {
+            it.key * it.value * (mapNumberTime[it.key] ?: 1)
+        }.reduce { acc, value ->
+            acc + value
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
